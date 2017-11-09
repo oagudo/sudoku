7d404fe21571d92cc630ec5913e0a63b28299ac6
oagudo <omar.agudo@gmail.com>
2017-11-09 15:50:06 +0100

    Refactoring and Board initialization

--- src/main/scala/model/Board.scala
+++ src/main/scala/model/Board.scala
@@ -1,12 +1,30 @@
 package model
 
-sealed trait Position
-case class Empty() extends Position
-case class Fill(n : Short) extends Position {
+case class Position(x : Int, y : Int)
+
+sealed trait Cell
+case class Empty() extends Cell
+case class Fill(n : Short) extends Cell {
   require(n >= 1 && n <= 9)
 }
 
 class Board() {
-  val positions = Array.ofDim[Empty](9, 9)
+  private val cells = Array.ofDim[Cell](9, 9)
+
+  val positions = for {
+    x <- 0 until 9
+    y <- 0 until 9
+  } yield Position(x, y)
+
+  positions.foreach(setCell(Empty(), _))
+
+  def setCell(c : Cell, p : Position): Unit = {
+    cells(p.x)(p.y) = c
+  }
+
+  def getCell(p : Position): Cell = {
+    return cells(p.x)(p.y)
+  }
+
 
 }
--- src/test/scala/model/BoardTest.scala
+++ src/test/scala/model/BoardTest.scala
@@ -1,28 +1,29 @@
 package model
 
-import org.scalatest.{FunSpec, Matchers}
 import org.scalatest.prop.Checkers
+import org.scalatest.{FunSpec, Matchers}
 
 class BoardTest extends FunSpec with Checkers with Matchers {
   describe("A Board") {
 
-    it("should have positions") {
-      val s = new Board()
-      s.positions
+    it("should initialize all cells to empty") {
+      val b = new Board()
+      assert(b.positions.forall(b.getCell(_) == Empty()))
     }
   }
 
-  describe("A Position") {
+
+  describe("A Cell") {
 
     it("can be empty") {
       Empty()
     }
 
-    it("can be filled with a number between 1 and 9") {
+    it("can be filled with a number between range [1 .. 9]") {
       Fill(1)
     }
 
-    it("cannot be filled with a number between 1 and 9") {
+    it("cannot be filled with a number out of range [1 .. 9]") {
       an [IllegalArgumentException] shouldBe thrownBy {
         Fill(0)
       }
