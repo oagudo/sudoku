510eeedff9280b581034080a43d557775490acdd
oagudo <omar.agudo@gmail.com>
2017-11-09 15:55:46 +0100

    Add sudoku rule engine

---
+++ src/test/scala/rule/SudokuTest.scala
@@ -0,0 +1,15 @@
+package rule
+
+import model.Board
+import org.scalatest.prop.Checkers
+import org.scalatest.{FunSpec, Matchers}
+
+class SudokuTest extends FunSpec with Checkers with Matchers {
+  describe("A Sudoku") {
+
+    it("is valid when all board cells are empty") {
+      val b = new Board()
+      assert(Sudoku(b).isValid())
+    }
+  }
+}
