d0fd3f6afd0765f9d1704d1ab51dfe22ed1967b2
oagudo <omar.agudo@gmail.com>
2017-11-09 16:30:14 +0100

    Add trivial ugly implementation. Time to refactor!

--- src/main/scala/rule/Sudoku.scala
+++ src/main/scala/rule/Sudoku.scala
@@ -6,16 +6,30 @@ object Sudoku {
 
   def isValid(b : Board) : Boolean = {
 
-    def getFilledCells(row : Int) : List[Fill] = {
+    def getFilledRowCells(row : Int) : List[Fill] = {
       val rowCells = (0 until 9).map(col => b.getCell(Position(row, col)))
       rowCells.collect {case c : Fill => c}.toList
     }
 
+    def getFilledColCells(col : Int) : List[Fill] = {
+      val colCells = (0 until 9).map(row => b.getCell(Position(row, col)))
+      colCells.collect {case c : Fill => c}.toList
+    }
+
     val rows = for {
       row <- 0 until 9
-    } yield getFilledCells(row)
+    } yield getFilledRowCells(row)
 
-    rows.forall(r => r.toSet.size == r.size)
+    if (!rows.forall(r => r.toSet.size == r.size))
+      return false
 
+    val columns = for {
+      col <- 0 until 9
+    } yield getFilledColCells(col)
+
+    if (!columns.forall(c => c.toSet.size == c.size))
+      return false
+
+    return true
   }
 }
