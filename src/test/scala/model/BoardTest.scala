2a1a28ac36edf1a069a0c1f40b38e1382696a45f
oagudo <omar.agudo@gmail.com>
2017-11-09 17:01:31 +0100

    Add test for getting small squares in Board

--- src/test/scala/model/BoardTest.scala
+++ src/test/scala/model/BoardTest.scala
@@ -10,6 +10,42 @@ class BoardTest extends FunSpec with Checkers with Matchers {
       val b = new Board()
       assert(b.positions.forall(b.getCell(_) == Empty()))
     }
+
+    it("is divided into 9 small squares which can be retrieved") {
+      val b = new Board()
+
+      // First small square
+      b.setCell(Fill(1), Position(0,0))
+      b.setCell(Fill(2), Position(1,1))
+      b.setCell(Fill(3), Position(2,2))
+
+      // Fourth small square
+      b.setCell(Fill(4), Position(3,0))
+      b.setCell(Fill(5), Position(4,1))
+      b.setCell(Fill(6), Position(5,2))
+
+      // Nineth small square
+      b.setCell(Fill(7), Position(6,6))
+      b.setCell(Fill(8), Position(7,7))
+      b.setCell(Fill(9), Position(8,8))
+
+      val squares = b.getSquares()
+
+      // First small square
+      assert(squares(0)(0) == Fill(1))
+      assert(squares(0)(4) == Fill(2))
+      assert(squares(0)(8) == Fill(3))
+
+      // Fourth small square
+      assert(squares(3)(0) == Fill(4))
+      assert(squares(3)(4) == Fill(5))
+      assert(squares(3)(8) == Fill(6))
+
+      // Nineth small square
+      assert(squares(8)(0) == Fill(6))
+      assert(squares(8)(4) == Fill(7))
+      assert(squares(8)(8) == Fill(8))
+    }
   }
 
   describe("A Cell") {
