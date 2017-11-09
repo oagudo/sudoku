83d996bd0492bcda1d67a05ab6f407d0c6449390
oagudo <omar.agudo@gmail.com>
2017-11-09 16:21:50 +0100

    First implementation for non repeated in row rule

--- src/main/scala/rule/Sudoku.scala
+++ src/main/scala/rule/Sudoku.scala
@@ -1,7 +1,21 @@
 package rule
 
-import model.Board
+import model._
 
 object Sudoku {
-  def isValid(b : Board) : Boolean = true
+
+  def isValid(b : Board) : Boolean = {
+
+    def getFilledCells(row : Int) : List[Fill] = {
+      val rowCells = (0 until 9).map(col => b.getCell(Position(row, col)))
+      rowCells.collect {case c : Fill => c}.toList
+    }
+
+    val rows = for {
+      row <- 0 until 9
+    } yield getFilledCells(row)
+
+    rows.forall(r => r.toSet.size == r.size)
+
+  }
 }
