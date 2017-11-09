deadfe0abb767043b235aeb15e2a2630200b5de7
oagudo <omar.agudo@gmail.com>
2017-11-09 15:17:33 +0100

    Start TDD

---
+++ src/test/scala/model/BoardTest.scala
@@ -0,0 +1,14 @@
+package model
+
+import org.scalatest.FunSpec
+import org.scalatest.prop.Checkers
+
+class BoardTest extends FunSpec with Checkers {
+  describe("A Board") {
+
+    it("should have positions") {
+      val s = new Board()
+      s.positions
+    }
+  }
+}
