58a5742f130c1d47850d44d19b08108337d5ef80
oagudo <omar.agudo@gmail.com>
2017-11-09 15:31:36 +0100

    Require good numbers implementation

--- src/main/scala/model/Board.scala
+++ src/main/scala/model/Board.scala
@@ -2,7 +2,9 @@ package model
 
 sealed trait Position
 case class Empty() extends Position
-case class Fill(n : Short) extends Position
+case class Fill(n : Short) extends Position {
+  require(n >= 1 && n <= 9)
+}
 
 class Board() {
   val positions = Array.ofDim[Empty](9, 9)
