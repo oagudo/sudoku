1b3e3702197b399ae13b402bc0929790bb052b16
oas <omar.agudo@gmail.com>
2018-02-19 14:14:28 +0100

    Add sudoku solver based on backtracking

--- src/main/scala/solver/SudokuBruteForceSolver.scala
+++ src/main/scala/solver/SudokuBruteForceSolver.scala
@@ -1,45 +1,42 @@
 package solver
 
-import model.{Board, Empty, Fill}
+import model.{Board, Empty, Fill, Position}
 import rule.Sudoku
 
 /**
   *  Brute force solver for sudokus
   *
-  *  Current implementation works for Sudokus where
-  *  there is just one possible solution
+  *  Backtracking implementation for solving sudokus
  */
 object SudokuBruteForceSolver {
 
-  private def AnyEmptyCell(b : Board) : Boolean = {
-    !b.positions.forall(b.getCell(_) != Empty())
+  private def emptyPositions(b : Board) : List[Position] = {
+    b.positions.filter(b.getCell(_) == Empty()).toList
   }
 
-  def solve(b : Board) : Unit = {
+  private def anyEmptyCell(b : Board) : Boolean = {
+    emptyPositions(b).length > 0
+  }
 
-    while (AnyEmptyCell(b)) {
+  def backtrackingSolver(b : Board) : Boolean = {
 
-      val emptyPositions = b.positions.filter(b.getCell(_) == Empty())
+    if (!anyEmptyCell(b))
+      return true
 
-      for (emptyPos <- emptyPositions) {
+    val emptyPosition = emptyPositions(b)(0)
 
-        var goodSolutions = List[Short]()
+    for (i <- (1 to 9)) {
 
-        for( n <- 1 to 9){
-          b.setCell(Fill(n.toShort), emptyPos)
-          if (Sudoku(b)) {
-            goodSolutions = goodSolutions ++ List(n.toShort)
-          }
-        }
+      b.setCell(Fill(i.toShort), emptyPosition)
 
-        if (goodSolutions.length == 1) { // Only when 1 possible solution is good !
-          b.setCell(Fill(goodSolutions(0)), emptyPos)
-        } else {
-          b.setCell(Empty(), emptyPos)
-        }
-
+      if (Sudoku(b) && backtrackingSolver(b)) {
+            return true
       }
-
+      // Undo the current cell for backtracking
+      b.setCell(Empty(), emptyPosition)
     }
+    return false
   }
+
+  def solve(b : Board) : Unit = backtrackingSolver(b)
 }
--- src/test/scala/solver/SudokuBruteForceSolverTest.scala
+++ src/test/scala/solver/SudokuBruteForceSolverTest.scala
@@ -8,7 +8,7 @@ import solver.SudokuBruteForceSolver
 class SudokuBruteForceSolverTest extends FunSpec with Checkers with Matchers {
   describe("A Sudoku solver") {
 
-    it("solves a one single solution sudoku correctly") {
+    it("solves an easy sudoku correctly") {
 
       val sudoku =  """5  9  3     2  ?  6     ?  ?  ?
                       |?  ?  ?     5  9  ?     ?  ?  6
@@ -43,7 +43,7 @@ class SudokuBruteForceSolverTest extends FunSpec with Checkers with Matchers {
     }
 
 
-    it("solves another one single solution sudoku correctly") {
+    it("solves another easy sudoku correctly") {
 
       val sudoku =  """?  ?  9     7  6  1     ?  ?  ?
                       |4  2  8     ?  ?  9     6  ?  7
@@ -72,6 +72,90 @@ class SudokuBruteForceSolverTest extends FunSpec with Checkers with Matchers {
                        |3  1  7     6  8  4     9  5  2
                        |2  9  5     3  1  7     8  4  6
                        |6  8  4     2  9  5     1  7  3""".stripMargin
+
+
+      val sudokuSolution = SudokuReader.read(solution)
+
+      assert(b == sudokuSolution)
+    }
+
+    it("solves a difficult sudoku") {
+
+      val empty =  """?  ?  ?     ?  ?  ?     ?  ?  ?
+                      |?  ?  ?     ?  ?  ?     ?  ?  ?
+                      |?  ?  ?     ?  ?  ?     ?  ?  ?
+                      |
+                      |?  ?  ?     ?  ?  ?     ?  ?  ?
+                      |?  ?  ?     ?  ?  ?     ?  ?  ?
+                      |?  ?  ?     ?  ?  ?     ?  ?  ?
+                      |
+                      |?  ?  ?     ?  ?  ?     ?  ?  ?
+                      |?  ?  ?     ?  ?  ?     ?  ?  ?
+                      |?  ?  ?     ?  ?  ?     ?  ?  ?""".stripMargin
+
+      val sudoku =  """?  ?  ?     ?  ?  ?     4  ?  ?
+                      |5  9  ?     ?  ?  ?     ?  ?  3
+                      |?  ?  1     9  ?  ?     ?  7  ?
+                      |
+                      |?  2  ?     ?  3  ?     ?  ?  ?
+                      |?  7  9     ?  ?  8     ?  ?  ?
+                      |?  ?  ?     ?  ?  4     ?  8  2
+                      |
+                      |?  8  ?     ?  ?  6     ?  ?  ?
+                      |3  ?  5     ?  ?  ?     ?  ?  ?
+                      |?  1  2     ?  5  3     ?  ?  8""".stripMargin
+
+      val b = SudokuReader.read(sudoku)
+
+      SudokuBruteForceSolver.solve(b)
+
+      val solution = """7  6  8   3  1  2   4  5  9
+                       |5  9  4   6  8  7   1  2  3
+                       |2  3  1   9  4  5   8  7  6
+                       |
+                       |8  2  6   5  3  1   9  4  7
+                       |4  7  9   2  6  8   3  1  5
+                       |1  5  3   7  9  4   6  8  2
+                       |
+                       |9  8  7   1  2  6   5  3  4
+                       |3  4  5   8  7  9   2  6  1
+                       |6  1  2   4  5  3   7  9  8""".stripMargin
+
+
+      val sudokuSolution = SudokuReader.read(solution)
+
+      assert(b == sudokuSolution)
+    }
+
+    it("solves another difficult sudoku") {
+
+      val sudoku = """?  ?  ?     ?  9  ?     ?  ?  ?
+                     |3  ?  ?     ?  ?  ?     8  6  ?
+                     |?  ?  6     5  ?  4     ?  ?  ?
+                     |
+                     |?  9  ?     ?  ?  ?     ?  ?  ?
+                     |?  7  1     ?  5  9     ?  4  ?
+                     |?  ?  4     ?  6  ?     ?  ?  5
+                     |
+                     |4  5  ?     ?  1  ?     ?  7  ?
+                     |?  ?  7     ?  ?  ?     2  3  1
+                     |?  ?  ?     ?  ?  ?     ?  ?  ?""".stripMargin
+
+      val b = SudokuReader.read(sudoku)
+
+      SudokuBruteForceSolver.solve(b)
+
+      val solution = """7  1  2    6  9  8    4  5  3
+                       |3  4  5    2  7  1    8  6  9
+                       |9  8  6    5  3  4    1  2  7
+                       |
+                       |5  9  8    4  2  3    7  1  6
+                       |6  7  1    8  5  9    3  4  2
+                       |2  3  4    1  6  7    9  8  5
+                       |
+                       |4  5  9    3  1  2    6  7  8
+                       |8  6  7    9  4  5    2  3  1
+                       |1  2  3    7  8  6    5  9  4""".stripMargin
 
 
       val sudokuSolution = SudokuReader.read(solution)
